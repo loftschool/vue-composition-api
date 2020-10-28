f7a5c3a65a2504aa7f607c3d0bf0f20995787a24
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-10-28 17:01:19 +0300

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>John</button>
+  <button>Alex</button>
+  <button>Mary</button>
+
+  <ul class="respos">
+    <li></li>
+  </ul>
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
