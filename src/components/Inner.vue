b0f0efedeabb0c49b0a4a1b22de5438c94f5ff8b
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-10-28 17:59:28 +0300

    написал функционал фильтрации и сортировки

---
+++ src/components/Inner.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <button @click="changeFilter('all')">*</button>
+    <button @click="changeFilter('react')">React</button>
+    <button @click="changeFilter('vue')">Vue</button>
+    <button @click="changeFilter('angular')">Angular</button>
+  </div>
+  <br />
+  <div>
+    <button @click="showUnProcessedItems">all</button>
+    <button @click="sortItems">sort</button>
+    <button @click="filterItems">filter</button>
+  </div>
+
+  <ul class="respos">
+    <li v-for="repo in processedRepos">
+      <span> {{ repo.full_name }} </span> <b v-if="repo.private">(private)</b>
+    </li>
+  </ul>
+</template>
+
+<script>
+import data from "../data.json";
+export default {
+  name: "Inner",
+  data() {
+    return {
+      repos: [],
+      processedRepos: [],
+      filter: "all",
+    };
+  },
+  computed: {
+    categorisedRepos() {
+      return this.repos[this.filter].items;
+    },
+  },
+  methods: {
+    changeFilter(filterName) {
+      this.filter = filterName;
+    },
+
+    flatenTheRepos(reposCollection) {
+      const result = [];
+      for (const reposCategory in reposCollection) {
+        result.push(reposCollection[reposCategory].items);
+      }
+      return result.flat();
+    },
+    filterItems() {
+      this.processedRepos = this.categorisedRepos.filter(
+        (repo) => repo.private
+      );
+    },
+    showUnProcessedItems() {
+      this.processedRepos = this.categorisedRepos;
+    },
+    sortItems() {
+      this.processedRepos = [...this.categorisedRepos].sort((a, b) => {
+        if (a.forks > b.forks) {
+          return -1;
+        }
+        if (a.forks < b.forks) {
+          return 1;
+        }
+        return 0;
+      });
+    },
+  },
+  watch: {
+    filter(value) {
+      switch (value) {
+        case "all":
+          this.processedRepos = this.flatenTheRepos(this.repos);
+          break;
+        default:
+          this.processedRepos = this.categorisedRepos;
+      }
+    },
+  },
+  mounted() {
+    this.repos = data;
+    this.processedRepos = this.flatenTheRepos(this.repos);
+  },
+};
+</script>
